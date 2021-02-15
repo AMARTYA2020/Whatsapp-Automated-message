9f0f3a27c2cbae043937b46c6fdd0997a69e7009
Amartya pandey <amartyapandey19@gmail.com>
2021-02-15 14:27:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
+# A very simple python project to automate messages to a particular person in whatsapp
+
 import pywhatkit
 pywhatkit.sendwhatmsg('+916299923311','Hey there automating whatsapp messages haha :)',15,23)
